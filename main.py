7ab902afa6a4f9c21d763981a82389bf97d1e73a
Wolfverus <75195899+WolfverusWasTaken@users.noreply.github.com>
2021-05-07 15:44:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import numpy as np
 import cv2
 from tkinter import filedialog
 
-import tkinter as tk
-from PIL import ImageTk,Image
+# import tkinter as tk
+# from PIL import ImageTk,Image
 
 image = filedialog.askopenfilename()
 image = cv2.imread(image)
+
+
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# image = Image.fromarray(image)
+# image = ImageTk.PhotoImage(image)
 
 def nothing(x):
     pass
@@ -26,12 +31,12 @@ def creategui():
 
 
 def setdefaultvalues():
-    # Set default value for max BGR trackbars.
+    # Set default value for MAX HSV trackbars.
     cv2.setTrackbarPos('BMax', 'image', 255)
     cv2.setTrackbarPos('GMax', 'image', 255)
     cv2.setTrackbarPos('RMax', 'image', 255)
 
-    # Set default value for min BGR trackbars.
+    # Set default value for MIN HSV trackbars.
     cv2.setTrackbarPos('BMin', 'image', 0)
     cv2.setTrackbarPos('GMin', 'image', 0)
     cv2.setTrackbarPos('RMin', 'image', 0)
@@ -41,6 +46,7 @@ creategui()
 
 setdefaultvalues()
 
+# Initialize to check if BGR min/max value changes
 B_Min = G_Min = R_Min = B_Max = G_Max = R_Max = 0
 
 output = image
@@ -48,6 +54,14 @@ wait_time = 33
 
 while True:
 
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # image = Image.fromarray(image)
+    # image = cv2.imread(image)
+
+    # image.putalpha(100)
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # get current positions of all trackbars
     B_Min = cv2.getTrackbarPos('BMin', 'image')
     G_Min = cv2.getTrackbarPos('GMin', 'image')
     R_Min = cv2.getTrackbarPos('RMin', 'image')
@@ -56,23 +70,17 @@ while True:
     G_Max = cv2.getTrackbarPos('GMax', 'image')
     R_Max = cv2.getTrackbarPos('RMax', 'image')
 
-    # Set min and max BGR values to display
+    # Set minimum and max BGR values to display
     lower = np.array([B_Min, G_Min, R_Min])
     upper = np.array([B_Max, G_Max, R_Max])
 
     mask = cv2.inRange(image, lower, upper)
     output = cv2.bitwise_and(image, image, mask=mask)
 
-
-
     # Display output image
     cv2.imshow('image', output)
-
-
 
     if cv2.waitKey(wait_time) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
-
-
